050f7462d504e673f2fe6b7a2a5e7c2575e1a446
radek <radek@HOME.Home>
2024-02-10 16:19:43 +0000

    Remove cmake files

--- HashTable/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radek/Documents/Coding/DSA-in-C/HashTable
-# Build directory: /home/radek/Documents/Coding/DSA-in-C/HashTable
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(HashTableTests "/home/radek/Documents/Coding/DSA-in-C/HashTable/hash_table_tests")
-set_tests_properties(HashTableTests PROPERTIES  _BACKTRACE_TRIPLES "/home/radek/Documents/Coding/DSA-in-C/HashTable/CMakeLists.txt;23;add_test;/home/radek/Documents/Coding/DSA-in-C/HashTable/CMakeLists.txt;0;")
